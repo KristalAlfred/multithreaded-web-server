310dc99ca0896b9eeb126f18235436f659bd1de2
Alfred Kristal Ern <alfred.kristal@gmail.com>
2022-11-18 13:54:54 +0100

    Serve index page on all requests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use std::net::TcpListener;
+use std::{
+    net::{TcpListener, TcpStream}, 
+    io::{BufReader, BufRead, Write}, fs
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
     listener.incoming().for_each(|stream| {
-        println!("New connection: {:?}", stream.unwrap());
+        handle_connection(stream.unwrap());
     });
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_req: Vec<_> = buf_reader
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let html = fs::read_to_string("index.html").unwrap();
+    let length = html.len();
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {length}\r\n\r\n{html}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
